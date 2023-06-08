f58e117d0d10ffc1f2aa502755351e181b16619b
mabustillo14 <mabustillo02@gmail.com>
2023-06-08 16:54:28 -0300

    [ADD] AI Model para conectarse a la api de OpenAI

---
+++ src/ai_model.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+
+# Cargar variables de entorno
+load_dotenv()
+
+OPENAI_KEY = os.getenv('OPENAI_KEY', '')
+
+# Asignar la API Key
+openai.api_key = OPENAI_KEY
+
+
+def mychatbot(query, contract_knowledge):
+    
+    # Enviar solicitud a la api OpenAI con el modelo "GPT-3.5-turbo"
+    res = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "Eres un asistente útil."},
+            {"role": "user", "content": "Responda mis consultas de acuerdo con el contexto dado. Si no se tiene una respuesta clara o sin información se debe indicar. \nContexto: {}".format(str(contract_knowledge))},
+            {"role": "assistant", "content": "¡Ok, seguro!"},
+            {"role": "user", "content": query}
+        ]
+    )
+
+    # Del diccionario extraer la informacion correspondiente al id "content"
+    conclusion = res['choices'][0]['message']['content']
+    
+    return conclusion
