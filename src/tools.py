900d900fc0802672e6da5f0405921b3bf8d81d88
mabustillo14 <mabustillo02@gmail.com>
2023-06-08 17:10:01 -0300

    [UPDATE] Comentar el funcionamiento de funciones y variables

--- src/tools.py
+++ src/tools.py
@@ -9,33 +9,31 @@ def contract_api(contrato, checkbox_categorias, pregunta):
 
     # Variables
     tot_prompt = "" # Todos los prompt que voy a solicitar
-    caracteres = 50 # Cantidad de caracteres para la respuesta por prompt
-    cant_caracteres = 0 
+    caracteres = 50 # Cantidad de caracteres para la respuesta por cada prompt seleccionado
+    cant_caracteres = 0 # Cantidad total de caracteres para la conclusion
     
-    # Extraer los datos de entrada
+    # Leer los datos de Input
     if len(checkbox_categorias) != 0: # Si hay alguna categoria seleccionada
-        for categoria in checkbox_categorias:
+        for categoria in checkbox_categorias: # Se extra el prompt de cada categoria
             tot_prompt += extract_prompt(categoria) + " \n"
             cant_caracteres += caracteres
         
     elif len(checkbox_categorias) == 0 and len(pregunta) == 0: 
-        # Si no se selecciona ninguna categoria y no hay una pregunta, devuelve la idea principal
+        # Si no se selecciona ninguna categoria y no hay una pregunta, se seleciona el prompt de la idea principal
         tot_prompt += extract_prompt("Idea Principal")
         cant_caracteres += caracteres
 
     
     if len(pregunta) != 0: # Verificar si hay una pregunta puntual
-        tot_prompt += " " + pregunta + " "
+        tot_prompt += " " + pregunta + ". " # Añadir pregunta como prompt
         cant_caracteres += caracteres
 
+    # Indicar que tantos caracteres debe tener la conclusion en funcion a la cantidad de prompts
     tot_prompt += " Dame un parrafo de maximo " + str(cant_caracteres) + " caracteres." 
 
     # Una vez definido los prompts, hacemos la consulta a nuestra base de conocimiento con la api de openAI
+    conclusion = " "
     conclusion = mychatbot(tot_prompt, contrato)
-    print(conclusion)
-
-    return conclusion
     
-
-
+    return conclusion
 
