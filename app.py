10ece9f081ed65fc220df579cff8a3304c10a212
mabustillo14 <mabustillo02@gmail.com>
2023-06-08 18:23:30 -0300

    [UPDATE] Agregar ejemplos default

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import gradio as gr
 from src.tools import contract_api
+from src.database import extract_contract
+
+
+def dar_ejemplo(contract_examples, contract_examples_categorias, contract_examples_question):
+    ejemplo = []
+    for i in range(len(contract_examples)):
+        aux_ejemplo = []
+        aux_ejemplo.append(extract_contract(contract_examples[i]))
+        aux_ejemplo.append(contract_examples_categorias[i])
+        aux_ejemplo.append(contract_examples_question[i])
+        ejemplo.append(aux_ejemplo)
+    return ejemplo
+
 
 # Descripción del Header
 title = 'Legal Insight.ai 💼'
@@ -17,15 +30,12 @@ article = """
 <br><center><b>Mario Bustillo 2023 🚀</b> | [Github](https://github.com/mabustillo14) | [Linkedin](https://www.linkedin.com/in/mario-bustillo/) 🤗</center>
 """
 
-enable_queue=False
-
-
-
 # Inputs
 contrato = gr.Textbox(label='1) Cargar Contrato 📂') # Texto
 checkbox_categorias =  gr.CheckboxGroup(
-    ["Idea Principal", "Plazos y Fechas", "Notas Importantes", "Pagos y Tarifas","Condiciones y Términos", "Letra Pequeña" ], 
-    label='2) Seleccionar Parámetros de Análisis ⚙️') # Lista
+    label='2) Seleccionar Parámetros de Análisis ⚙️', 
+    choices = ["Idea Principal", "Plazos y Fechas", "Notas Importantes", "Pagos y Tarifas","Condiciones y Términos", "Letra Pequeña" ], 
+    ) # Lista
 pregunta = gr.Textbox(label='3) Pregunta Puntual ❓ [OPCIONAL]') # Texto
 input = [contrato, checkbox_categorias, pregunta]
 
@@ -33,15 +43,12 @@ input = [contrato, checkbox_categorias, pregunta]
 conclusiones = gr.Textbox(label="Conclusiones del análisis del Contrato 📝🔍")
 output = [conclusiones]
 
-"""
+
 # Ejemplos precargados
-def dar_ejemplo():
-    with open('src/contrato.txt', 'r') as archivo:
-    # Lee el contenido completo del archivo
-        contrato = archivo.read()
-    ejemplo = [[contrato, ["Letra Pequeña"], "¿Cuánto es el aumento mensual?"]]
-    return ejemplo
-"""
+contract_examples = ["Alquilar Departamento", "Makers Coding Challenge", "Contrato Profesional"]
+contract_examples_categorias = [["Letra Pequeña"], ["Idea Principal"], []]
+contract_examples_question = ["¿Cuánto es el aumento mensual?","","¿De que tipo es mi jornada laboral?"]
+ejemplo_default = dar_ejemplo(contract_examples, contract_examples_categorias, contract_examples_question)
 
 # GUI
 demo = gr.Interface(
@@ -50,9 +57,8 @@ demo = gr.Interface(
     outputs=output,
     title=title,
     description=description, 
-    article=article
-#    examples = dar_ejemplo()
+    article=article,
+    examples = ejemplo_default
     )
-    
 
-demo.launch(enable_queue=enable_queue,debug=True)
+demo.launch(enable_queue=False,debug=True)
